16227bcc2b740e29681fa92dff579014dd063646
ferbertigo <I_Ferbert@gmx.de>
2024-07-05 08:55:30 +0200

    Ticket: fixed core color

--- lib/presantation/scopes_page/widgets/scopes_board_item.dart
+++ lib/presantation/scopes_page/widgets/scopes_board_item.dart
@@ -14,8 +14,11 @@ class ScoreBoardItemWidget extends StatelessWidget {
       2: Colors.orange,
       3: Colors.black};
 
-    Color? chooseColor(int place) {
+    Color? chooseColor(int place, int currentScore) {
       Color? result;
+      if(currentScore==0){
+        return myMap[3];
+      }
       if (place >= 4) {
         return myMap[3];
       }
@@ -34,14 +37,14 @@ class ScoreBoardItemWidget extends StatelessWidget {
             Text(
               '${score.currentPosition}#',
               textAlign: TextAlign.left,
-              style: themeData.textTheme.bodyLarge?.copyWith(color: chooseColor(score.currentPosition)) ,
+              style: themeData.textTheme.bodyLarge?.copyWith(color: chooseColor(score.currentPosition,score.score)) ,
             ),
             Padding(
               padding: const EdgeInsets.only(left: 8.0),
               child: Text(
                 score.playerName,
                 textAlign: TextAlign.center,
-                style:  themeData.textTheme.bodyLarge?.copyWith(color: chooseColor(score.currentPosition)) ,
+                style:  themeData.textTheme.bodyLarge?.copyWith(color: chooseColor(score.currentPosition, score.score)) ,
               ),
             ),
           ],
@@ -50,7 +53,7 @@ class ScoreBoardItemWidget extends StatelessWidget {
           padding: const EdgeInsets.only(right: 4.0),
           child: Text(
             '${score.score}',
-            style: themeData.textTheme.bodyLarge?.copyWith(color: chooseColor(score.currentPosition)) ,
+            style: themeData.textTheme.bodyLarge?.copyWith(color: chooseColor(score.currentPosition, score.score)) ,
           ),
         )
       ],
